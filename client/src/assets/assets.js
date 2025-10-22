d2601664217cb65a1cb31ddf1b8f680dbd67fb21
Anshuman <anshumanpanda481@gmail.com>
2025-10-22 11:46:18 +0530

    update

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './logo.png';
+import logo from './logo.PNG';
 import marvelLogo from './marvelLogo.svg';
 import googlePlay from './googlePlay.svg';
 import appStore from './appStore.svg';
